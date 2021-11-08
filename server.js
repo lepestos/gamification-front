eb0ca5f551347e026060646b1947b60276af2a35
Kirill Nazarov <kirillnazarov1928@gmail.com>
2021-11-08 11:33:30 +0300

    fixed router

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+app.route('/*')
+    .get(function(req, res) {
+          res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+module.exports = app;
